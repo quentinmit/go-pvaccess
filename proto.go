3349ef760968451b6e01efdae23cc31adcaf0d05
Quentin Smith <quentin@mit.edu>
2019-09-28 04:45:54 -0400

    Message header mostly implemented

---
+++ proto.go
@@ -0,0 +1,56 @@
+package pvaccess
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/quentinmit/go-pvaccess/pvdata"
+)
+
+// TODO: Messages MUST be aligned on a 64-bit boundary (Pad with zeros???)
+
+type pvAccessHeader struct {
+	Magic          pvdata.PVByte
+	Version        pvdata.PVByte
+	Flags          pvdata.PVByte
+	MessageCommand pvdata.PVByte
+	PayloadSize    pvdata.PVInt
+}
+
+func (v *pvAccessHeader) PVEncode(s *EncoderState) error {
+	if err := v.Magic.PVEncode(s); err != nil {
+		return err
+	}
+	if err := v.Version.PVEncode(s); err != nil {
+		return err
+	}
+	if err := v.Flags.PVEncode(s); err != nil {
+		return err
+	}
+	return v.PayloadSize.PVEncode(s)
+}
+func (v *pvAccessHeader) PVDecode(s *EncoderState) error {
+	if err := v.Magic.PVDecode(s); err != nil {
+		return err
+	}
+	if v.Magic != 0xCA {
+		return fmt.Errorf("unexpected magic %x", v.Magic)
+	}
+	if err := v.Version.PVDecode(s); err != nil {
+		return err
+	}
+	if err := v.Flags.PVDecode(s); err != nil {
+		return err
+	}
+	if err := v.MessageCommand.PVDecode(s); err != nil {
+		return err
+	}
+	// Need to decode flags before decoding PayloadSize
+	// TODO: Ignore these flags if Set byte order message told us to
+	if v.Flags & 0x70 {
+		s.ByteOrder = binary.BigEndian
+	} else {
+		s.ByteOrder = binary.LittleEndian
+	}
+	return v.PayloadSize.PVDecode(s)
+}
