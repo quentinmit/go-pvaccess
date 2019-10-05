dc6515e1a9fe079e567d7a1732ab3ad8972d156c
Quentin Smith <quentin@mit.edu>
2019-10-05 03:25:31 -0400

    Send search replies from the beacon port, and refactor search in preparation for supporting TCP search

--- internal/search/search.go
+++ internal/search/search.go
@@ -19,10 +19,10 @@ type server struct {
 	lastBeacon proto.BeaconMessage
 }
 
-const udpPort = 5076
-
 const startupInterval = time.Second
 const startupCount = 15
+
+// TODO: EPICS_PVA_BEACON_PERIOD environment variable
 const beaconInterval = 5 * time.Second
 
 // Serve transmits beacons and listens for searches on every interface on the machine.
@@ -34,8 +34,7 @@ func Serve(ctx context.Context, serverAddr *net.TCPAddr) error {
 		return err
 	}
 	if len(serverAddr.IP) > 0 {
-		// TODO: How should IPv4 addresses be aligned?
-		copy(beacon.ServerAddress[:], serverAddr.IP)
+		copy(beacon.ServerAddress[:], serverAddr.IP.To16())
 	}
 	beacon.ServerPort = uint16(serverAddr.Port)
 	beacon.Protocol = "tcp"
@@ -51,71 +50,25 @@ func Serve(ctx context.Context, serverAddr *net.TCPAddr) error {
 	//   Listen on 224.0.0.128:5076
 	//   IP_ADD_MEMBERSHIP 224.0.0.128, 127.0.0.1
 
-	sendConn, err := udpconn.NewMulti()
+	ln, err := udpconn.Listen(ctx)
 	if err != nil {
 		return err
 	}
-	beaconSender := connection.New(sendConn, proto.FLAG_FROM_SERVER)
+
+	beaconSender := connection.New(ln.BroadcastConn(), proto.FLAG_FROM_SERVER)
 	beaconSender.Version = pvdata.PVByte(2)
 
-	var ips []*net.UDPAddr
-	var bcasts []*net.UDPAddr
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		return err
-	}
+	ctxlog.L(ctx).Infof("sending beacons to %v", ln.BroadcastSendAddresses())
 
-	for _, i := range interfaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			return err
+	go func() {
+		if err := (&searchServer{
+			GUID:       beacon.GUID,
+			ServerPort: serverAddr.Port,
+		}).serve(ctx, ln); err != nil && err != io.EOF {
+			ctxlog.L(ctx).Errorf("failed handling search request: %v", err)
 		}
-		for _, addr := range addrs {
-			if addr, ok := addr.(*net.IPNet); ok {
-				laddr := &net.UDPAddr{
-					IP:   addr.IP,
-					Port: udpPort,
-				}
-				if addr.IP.To4() == nil {
-					laddr.Zone = i.Name
-					// TODO: Figure out what IPv6 multicast group we should use.
-					continue
-				}
-				ips = append(ips, laddr)
+	}()
 
-				var extra []*net.UDPAddr
-
-				if i.Flags&net.FlagBroadcast == net.FlagBroadcast {
-					laddr := &net.UDPAddr{
-						IP:   bcastIP(addr.IP, addr.Mask),
-						Port: laddr.Port,
-						Zone: laddr.Zone,
-					}
-					ips = append(ips, laddr)
-					bcasts = append(bcasts, laddr)
-					extra = append(extra, laddr)
-				}
-				ctxlog.L(ctx).Debugf("Listening on %v + %v", laddr, extra)
-
-				go func() {
-					if err := (&searchServer{
-						GUID:       beacon.GUID,
-						ServerPort: serverAddr.Port,
-					}).serve(ctx, laddr, extra); err != nil && err != io.EOF {
-						ctxlog.L(ctx).Errorf("failed handling search request: %v", err)
-					}
-				}()
-			}
-		}
-	}
-	sendConn.SendAddresses = bcasts
-	ctxlog.L(ctx).Infof("sending beacons to %v", bcasts)
-	ctxlog.L(ctx).Infof("listening on %v", ips)
-	/*
-		go (&searchServer{
-			GUID: beacon.GUID,
-		}).serve(ctx, &net.UDPAddr{Port: udpPort})
-	*/
 	ticker := time.NewTicker(startupInterval)
 	defer func() { ticker.Stop() }()
 	i := 0
@@ -135,45 +88,26 @@ func Serve(ctx context.Context, serverAddr *net.TCPAddr) error {
 	}
 }
 
-func bcastIP(ip net.IP, mask net.IPMask) net.IP {
-	if len(mask) == net.IPv4len {
-		ip = ip.To4()
-	}
-	var bcastip net.IP
-	for i := range mask {
-		bcastip = append(bcastip, ip[i]|(^mask[i]))
-	}
-	return bcastip
-}
-
 type searchServer struct {
 	GUID       [12]byte
 	ServerPort int
 }
 
-func (s *searchServer) serve(ctx context.Context, laddr *net.UDPAddr, extra []*net.UDPAddr) (err error) {
+func (s *searchServer) serve(ctx context.Context, ln *udpconn.Listener) (err error) {
 	defer func() {
 		if err != nil {
 			ctxlog.L(ctx).Errorf("error listening for search requests: %v", err)
 		}
 	}()
-	ctx = ctxlog.WithField(ctx, "local_addr", laddr)
-	ln, err := udpconn.Listen(ctx, "udp", laddr)
-	if err != nil {
-		return err
-	}
 	defer ln.Close()
-	for _, e := range extra {
-		if err := ln.AddReadAddress(ctx, "udp", e); err != nil {
-			return err
-		}
-	}
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			return err
 		}
-		go s.handleConnection(ctx, laddr, conn)
+		laddr := conn.LocalAddr()
+		ctx = ctxlog.WithField(ctx, "local_addr", laddr)
+		go s.handleConnection(ctx, ln.LocalAddr(), conn)
 	}
 }
 
@@ -185,10 +119,7 @@ func (s *searchServer) handleConnection(ctx context.Context, laddr *net.UDPAddr,
 	}()
 	defer conn.Close()
 
-	ctx = ctxlog.WithFields(ctx, ctxlog.Fields{
-		"local_addr":  laddr,
-		"remote_addr": conn.Addr(),
-	})
+	ctx = ctxlog.WithField(ctx, "remote_addr", conn.Addr())
 
 	c := connection.New(conn, proto.FLAG_FROM_SERVER)
 	c.Version = pvdata.PVByte(2)
@@ -223,7 +154,6 @@ func (s *searchServer) handleConnection(ctx context.Context, laddr *net.UDPAddr,
 		}
 		if len(found) > 0 || req.Flags&proto.SEARCH_REPLY_REQUIRED == proto.SEARCH_REPLY_REQUIRED {
 			c.SendApp(ctx, proto.APP_SEARCH_RESPONSE, resp)
-			// TODO: Use sendConn
 			// TODO: Send response to req.ResponseAddr if set
 		}
 	}
--- internal/udpconn/udpconn.go
+++ internal/udpconn/udpconn.go
@@ -2,16 +2,22 @@
 package udpconn
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"io"
 	"net"
-	"sync"
+	"runtime"
 	"syscall"
 
 	"github.com/quentinmit/go-pvaccess/internal/ctxlog"
 )
+
+var mcastIP = net.IP{224, 0, 0, 128}
+
+// TODO: EPICS_PVA_BROADCAST_PORT environment variable
+const udpPort = 5076
 
 func ipv6LoopbackIndex(ctx context.Context) int {
 	interfaces, err := net.Interfaces()
@@ -31,28 +37,123 @@ func ipv6LoopbackIndex(ctx context.Context) int {
 	return -1
 }
 
+// Listener holds all the UDP sockets we're listening on.
+// We need a bunch of sockets.
+// One socket on INADDR_ANY with a random port to send beacons from
+// For each interface,
+//   Listen on addr:5076
+//     IP_MULTICAST_IF 127.0.0.1
+//     IP_MULTICAST_LOOP 1
+//   Listen on broadcast:5076 (if interface has broadcast flag)
+// One socket listening on 224.0.0.128 on lo
+//   Listen on 224.0.0.128:5076
+//   IP_ADD_MEMBERSHIP 224.0.0.128, 127.0.0.1
 type Listener struct {
-	lns    []*net.UDPConn
-	connCh chan *Conn
-	mu     sync.Mutex
-	conns  map[addr]*Conn
+	sendConn               *net.UDPConn
+	broadcastSendAddresses []*net.UDPAddr
+	lns                    []*net.UDPConn
+	connCh                 chan *Conn
 }
 
-func Listen(ctx context.Context, network string, laddr *net.UDPAddr) (*Listener, error) {
-	udpConn, network, err := listen(ctx, network, laddr.String())
+func Listen(ctx context.Context) (*Listener, error) {
+	sendConn, err := net.ListenUDP("udp", &net.UDPAddr{})
 	if err != nil {
-		return nil, fmt.Errorf("listen %v: %v", laddr, err)
+		return nil, err
 	}
 	ch := make(chan *Conn)
 	ln := &Listener{
-		lns:    []*net.UDPConn{udpConn},
-		conns:  make(map[addr]*Conn),
-		connCh: ch,
+		sendConn: sendConn,
+		lns:      []*net.UDPConn{sendConn},
+		connCh:   ch,
+	}
+	if err := ln.bindInterfaces(ctx); err != nil {
+		ln.Close()
+		return nil, err
+	}
+	if err := ln.bindMulticast(ctx); err != nil {
+		ln.Close()
+		return nil, err
+	}
+	var ips []*net.UDPAddr
+	for _, conn := range ln.lns {
+		ips = append(ips, conn.LocalAddr().(*net.UDPAddr))
+	}
+	ctxlog.L(ctx).Infof("Listening on %v", ips)
+	return ln, nil
+}
+
+func (ln *Listener) LocalAddr() *net.UDPAddr {
+	return ln.sendConn.LocalAddr().(*net.UDPAddr)
+}
+
+func (ln *Listener) bindInterfaces(ctx context.Context) error {
+	var ips []*net.UDPAddr
+	var bcasts []*net.UDPAddr
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return err
 	}
 
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return err
+		}
+		for _, addr := range addrs {
+			if addr, ok := addr.(*net.IPNet); ok {
+				laddr := &net.UDPAddr{
+					IP:   addr.IP,
+					Port: udpPort,
+				}
+				if addr.IP.To4() == nil {
+					laddr.Zone = i.Name
+					// TODO: Figure out what IPv6 multicast group we should use.
+					continue
+				}
+				ips = append(ips, laddr)
+
+				if err := ln.bindUnicast(ctx, laddr); err != nil {
+					return err
+				}
+
+				if i.Flags&net.FlagBroadcast == net.FlagBroadcast {
+					laddr := &net.UDPAddr{
+						IP:   bcastIP(addr.IP, addr.Mask),
+						Port: laddr.Port,
+						Zone: laddr.Zone,
+					}
+					ips = append(ips, laddr)
+					bcasts = append(bcasts, laddr)
+					if err := ln.bindBroadcast(ctx, laddr); err != nil {
+						return err
+					}
+				}
+			}
+		}
+	}
+	ln.broadcastSendAddresses = bcasts
+	return nil
+}
+
+func bcastIP(ip net.IP, mask net.IPMask) net.IP {
+	if len(mask) == net.IPv4len {
+		ip = ip.To4()
+	}
+	var bcastip net.IP
+	for i := range mask {
+		bcastip = append(bcastip, ip[i]|(^mask[i]))
+	}
+	return bcastip
+}
+
+func (ln *Listener) bindUnicast(ctx context.Context, laddr *net.UDPAddr) error {
+	udpConn, network, err := listen(ctx, "udp", laddr.String())
+	if err != nil {
+		return fmt.Errorf("listen %v: %v", laddr, err)
+	}
 	rawConn, err := udpConn.SyscallConn()
 	if err != nil {
-		return nil, fmt.Errorf("can't obtain fd: %v", err)
+		return fmt.Errorf("can't obtain fd: %v", err)
 	}
 	var cerr error
 	if err := rawConn.Control(func(fd uintptr) {
@@ -75,27 +176,62 @@ func Listen(ctx context.Context, network string, laddr *net.UDPAddr) (*Listener,
 			}
 		}
 	}); err != nil {
-		return nil, err
+		return err
 	}
 	if cerr != nil {
-		return nil, cerr
+		return cerr
 	}
-
-	go ln.readLoop(udpConn)
-	return ln, nil
+	return ln.addConn(ctx, udpConn)
 }
 
-func (ln *Listener) AddReadAddress(ctx context.Context, network string, laddr *net.UDPAddr) error {
-	udpConn, _, err := listen(ctx, network, laddr.String())
+func (ln *Listener) bindBroadcast(ctx context.Context, laddr *net.UDPAddr) error {
+	udpConn, _, err := listen(ctx, "udp", laddr.String())
 	if err != nil {
 		return fmt.Errorf("listen %v: %v", laddr, err)
 	}
+	return ln.addConn(ctx, udpConn)
+}
+
+func (ln *Listener) bindMulticast(ctx context.Context) error {
+	laddr := &net.UDPAddr{
+		IP:   mcastIP,
+		Port: udpPort,
+	}
+	if runtime.GOOS == "windows" {
+		laddr.IP = nil
+	}
+	udpConn, _, err := listen(ctx, "udp4", laddr.String())
+	if err != nil {
+		return fmt.Errorf("listen %v: %v", laddr, err)
+	}
+	rawConn, err := udpConn.SyscallConn()
+	if err != nil {
+		return fmt.Errorf("can't obtain fd: %v", err)
+	}
+	var cerr error
+	if err := rawConn.Control(func(fd uintptr) {
+		if err := syscall.SetsockoptIPMreq(int(fd), syscall.IPPROTO_IP, syscall.IP_ADD_MEMBERSHIP, &syscall.IPMreq{
+			Multiaddr: [4]byte{224, 0, 0, 128},
+			Interface: [4]byte{127, 0, 0, 1},
+		}); err != nil {
+			cerr = err
+		}
+	}); err != nil {
+		return err
+	}
+	if cerr != nil {
+		return cerr
+	}
+	return ln.addConn(ctx, udpConn)
+}
+
+func (ln *Listener) addConn(ctx context.Context, udpConn *net.UDPConn) error {
 	ln.lns = append(ln.lns, udpConn)
-	go ln.readLoop(udpConn)
+	go ln.readLoop(ctx, udpConn)
 	return nil
 }
 
-func (ln *Listener) readLoop(conn *net.UDPConn) error {
+func (ln *Listener) readLoop(ctx context.Context, conn *net.UDPConn) error {
 	for {
 		pkt := make([]byte, 1500)
 		n, ua, err := conn.ReadFromUDP(pkt)
@@ -103,19 +239,12 @@ func (ln *Listener) readLoop(conn *net.UDPConn) error {
 			return err
 		}
 		pkt = pkt[:n]
-		key := addr{
-			Port: ua.Port,
-			Zone: ua.Zone,
+		ln.connCh <- &Conn{
+			r:             bytes.NewReader(pkt),
+			w:             ln.sendConn,
+			sendAddresses: []*net.UDPAddr{ua},
+			laddr:         conn.LocalAddr().(*net.UDPAddr),
 		}
-		copy(key.IP[:], ua.IP)
-		ln.mu.Lock()
-		if _, ok := ln.conns[key]; !ok {
-			ln.conns[key] = ln.newConn(key, ua)
-			ln.connCh <- ln.conns[key]
-		}
-		conn := ln.conns[key]
-		ln.mu.Unlock()
-		conn.packetIn.Write(pkt)
 	}
 }
 
@@ -137,42 +266,34 @@ func (ln *Listener) Close() (err error) {
 	return
 }
 
-func (ln *Listener) closeAddr(key addr) error {
-	ln.mu.Lock()
-	defer ln.mu.Unlock()
-	delete(ln.conns, key)
-	return nil
+func (ln *Listener) BroadcastConn() *Conn {
+	return &Conn{
+		r:             &io.LimitedReader{N: 0},
+		w:             ln.sendConn,
+		sendAddresses: ln.broadcastSendAddresses,
+		laddr:         ln.sendConn.LocalAddr().(*net.UDPAddr),
+	}
 }
 
-type addr struct {
-	IP   [16]byte
-	Port int
-	Zone string
+func (ln *Listener) BroadcastSendAddresses() []*net.UDPAddr {
+	return ln.broadcastSendAddresses
 }
 
 type Conn struct {
-	addr     *net.UDPAddr
-	key      addr
-	packetIn io.WriteCloser
-
-	r  io.ReadCloser
-	ln *Listener
+	r             io.Reader
+	w             *net.UDPConn
+	sendAddresses []*net.UDPAddr
+	laddr         *net.UDPAddr
 }
 
-func (ln *Listener) newConn(key addr, addr *net.UDPAddr) *Conn {
-	conn := &Conn{
-		addr: addr,
-		key:  key,
-		ln:   ln,
-	}
-	r, w := io.Pipe()
-	conn.packetIn = w
-	conn.r = r
-	return conn
-}
-
+// Addr returns the remote address.
+// Meaningless if called on a broadcast socket.
 func (conn *Conn) Addr() *net.UDPAddr {
-	return conn.addr
+	return conn.sendAddresses[0]
+}
+
+func (conn *Conn) LocalAddr() *net.UDPAddr {
+	return conn.laddr
 }
 
 func (conn *Conn) Read(p []byte) (int, error) {
@@ -180,41 +301,21 @@ func (conn *Conn) Read(p []byte) (int, error) {
 }
 
 func (conn *Conn) Write(p []byte) (int, error) {
-	// Always write from the first Listener
-	return conn.ln.lns[0].WriteToUDP(p, conn.addr)
-}
-
-func (conn *Conn) Close() error {
-	conn.packetIn.Close()
-	return conn.ln.closeAddr(conn.key)
-}
-
-type MultiConn struct {
-	SendAddresses []*net.UDPAddr
-	conn          *net.UDPConn
-}
-
-func NewMulti() (*MultiConn, error) {
-	ln, err := net.ListenUDP("udp", &net.UDPAddr{})
-	if err != nil {
-		return nil, err
-	}
-	return &MultiConn{conn: ln}, nil
-}
-
-func (mc *MultiConn) Read(p []byte) (int, error) {
-	return mc.Read(p)
-}
-
-func (mc *MultiConn) Write(p []byte) (int, error) {
-	for _, addr := range mc.SendAddresses {
-		if _, err := mc.conn.WriteToUDP(p, addr); err != nil {
+	for _, addr := range conn.sendAddresses {
+		if _, err := conn.w.WriteToUDP(p, addr); err != nil {
 			return 0, err
 		}
 	}
 	return len(p), nil
 }
 
-func (mc *MultiConn) Close() error {
-	return mc.conn.Close()
+func (conn *Conn) Close() error {
+	return nil
+	// Don't close conn.w since it's shared by multiple Conns
+}
+
+// SetSendAddress overrides the address that the connection sends to.
+// This is used to handle CMD_ORIGIN_TAG.
+func (conn *Conn) SetSendAddress(addr *net.UDPAddr) {
+	conn.sendAddresses = []*net.UDPAddr{addr}
 }
